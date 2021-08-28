d216e44c406587f833eb980455c113817dc63eee
Stuart Jansen <sjansen@buscaluz.org>
2021-08-27 19:19:58 -0600

    reformat cloudfront/viewer-request/index.js

--- cloudfront/viewer-request/index.js
+++ cloudfront/viewer-request/index.js
@@ -1,102 +1,115 @@
 'use strict';
 
-import { createRemoteJWKSet } from 'jose/jwks/remote'
-import { jwtVerify } from 'jose/jwt/verify'
-import { JWTExpired } from 'jose/util/errors'
-import { parse } from "cookie";
+import {
+    createRemoteJWKSet
+} from 'jose/jwks/remote'
+import {
+    jwtVerify
+} from 'jose/jwt/verify'
+import {
+    JWTExpired
+} from 'jose/util/errors'
+import {
+    parse
+} from "cookie";
 
-import { JWKS_ENDPOINT, PUBLIC_PREFIXES } from "./config"
+import {
+    JWKS_ENDPOINT,
+    PUBLIC_PREFIXES
+} from "./config"
 
 const JWKS = createRemoteJWKSet(JWKS_ENDPOINT);
 
 exports.handler = async (e, c, cb) => {
-  const request = e.Records[0].cf.request;
-  const host = request.headers.host[0].value;
-  const uri = request.uri;
+    const request = e.Records[0].cf.request;
+    const host = request.headers.host[0].value;
+    const uri = request.uri;
 
-  if (uri === '/' || uri === '/favicon.ico' || uri.startsWith('/b/')) {
-    console.log(host, uri);
-    cb(null, request);
-    return;
-  }
-
-  for (let prefix of PUBLIC_PREFIXES) {
-    if (uri.startsWith(prefix)) {
-      console.log(host, uri, "Public prefix matched.");
-      cb(null, request);
-      return;
+    if (uri === '/' || uri === '/favicon.ico' || uri.startsWith('/b/')) {
+        console.log(host, uri);
+        cb(null, request);
+        return;
     }
-  }
 
-  if (!request.headers.cookie) {
-    console.log(host, uri, "Authentication required.")
-    refreshToken(uri, cb);
-    return;
-  }
-
-  const cookies = request.headers.cookie.reduce(
-    (reduced, header) => Object.assign(reduced, parse(header.value)),
-    {}
-  );
-  if (!cookies.auth_token) {
-    console.log(host, uri, "Missing auth_token.")
-    refreshToken(uri, cb);
-    return;
-  }
-
-  try {
-    const { payload, protectedHeader } = await jwtVerify(
-      cookies.auth_token, JWKS, {
-        issuer: 'bouncer',
-        audience: 'bouncer-authz',
-        subject: host,
-      }
-    )
-    //console.log(protectedHeader);
-    //console.log(payload);
-    console.log(host, uri, "Access granted.")
-    cb(null, request);
-    return;
-  } catch (err) {
-    if (err instanceof JWTExpired) {
-      console.log(host, uri, "Refreshing auth_token...")
-      refreshToken(uri, cb);
-      return;
+    for (let prefix of PUBLIC_PREFIXES) {
+        if (uri.startsWith(prefix)) {
+            console.log(host, uri, "Public prefix matched.");
+            cb(null, request);
+            return;
+        }
     }
-  }
 
-  console.log(host, uri, "Access denied.")
-  accessDenied(cb);
+    if (!request.headers.cookie) {
+        console.log(host, uri, "Authentication required.")
+        refreshToken(uri, cb);
+        return;
+    }
+
+    const cookies = request.headers.cookie.reduce(
+        (reduced, header) => Object.assign(reduced, parse(header.value)), {}
+    );
+    if (!cookies.auth_token) {
+        console.log(host, uri, "Missing auth_token.")
+        refreshToken(uri, cb);
+        return;
+    }
+
+    try {
+        const {
+            payload,
+            protectedHeader
+        } = await jwtVerify(
+            cookies.auth_token, JWKS, {
+                issuer: 'bouncer',
+                audience: 'bouncer-authz',
+                subject: host,
+            }
+        )
+        //console.log(protectedHeader);
+        //console.log(payload);
+        console.log(host, uri, "Access granted.")
+        cb(null, request);
+        return;
+    } catch (err) {
+        if (err instanceof JWTExpired) {
+            console.log(host, uri, "Refreshing auth_token...")
+            refreshToken(uri, cb);
+            return;
+        }
+    }
+
+    console.log(host, uri, "Access denied.")
+    accessDenied(cb);
 };
 
 function accessDenied(cb) {
-  const response = {
-    status: '403',
-    statusDescription: 'Forbidden',
-    headers: {
-      'cache-control': [{
-          key: 'Cache-Control',
-          value: 'no-cache'
-      }],
-      'content-type': [{
-          key: 'Content-Type',
-          value: 'text/plain'
-      }]
-    },
-    body: '403 Access Denied',
-  };
-  cb(null, response);
+    const response = {
+        status: '403',
+        statusDescription: 'Forbidden',
+        headers: {
+            'cache-control': [{
+                key: 'Cache-Control',
+                value: 'no-cache'
+            }],
+            'content-type': [{
+                key: 'Content-Type',
+                value: 'text/plain'
+            }]
+        },
+        body: '403 Access Denied',
+    };
+    cb(null, response);
 }
 
 function refreshToken(uri, cb) {
-  cb(null, {
-    status: '302',
-    statusDescription: 'Found',
-    headers: {
-        location: [{
-            key: 'Location',
-            value: `/b/redirect/?target=${encodeURIComponent(uri)}`,
-        }],
-    },
-  });
+    cb(null, {
+        status: '302',
+        statusDescription: 'Found',
+        headers: {
+            location: [{
+                key: 'Location',
+                value: `/b/redirect/?target=${encodeURIComponent(uri)}`,
+            }],
+        },
+    });
 }
